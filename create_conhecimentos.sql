5d0d2704174c9e4c5ebb0884ebbfd94eb37e4024
EsterGalati <esterscgalati@gmail.com>
2025-08-19 14:44:30 -0300

    Mudança de Interface, Refatorando Código

--- create_conhecimentos.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE conhecimentos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    materia TEXT NOT NULL,
-    topico TEXT NOT NULL,
-    explicacao TEXT NOT NULL
-);
--- main.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from bot import VestibulandoBot
-
-# Iniciar app
-app = VestibulandoBot()
-app.run()
