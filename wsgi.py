2e0e33e7679426cebb30120213e825e6907670e6
Caio Manoel Costa Leonardo <caio.m.c.leonardo@gmail.com>
2025-08-31 00:15:46 -0300

    Fix:Correção de Cors

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
+# wsgi.py
 from app import create_app
+
 app = create_app()
+# cookies p/ DEV
+app.config.update(
+    SESSION_COOKIE_SAMESITE="Lax",
+    SESSION_COOKIE_SECURE=False,
+)
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-    app.config.update(
-    SESSION_COOKIE_SAMESITE="None",
-    SESSION_COOKIE_SECURE=True,
-)
